ef6fe927eae574a5c863a67c42a337779d5ab1b4
elizavetaaaa <45116542+elizavetaaaa@users.noreply.github.com>
2019-05-13 21:01:01 +0600

    Add files via upload
    almost done

--- Main.java
+++ Main.java
@@ -1,35 +1,36 @@
 package com.company;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
-import java.io.*;
+import java.util.List;
 
 
 public class Main {
+   static JLabel title, title1;
+   static  JTextField input;
+   static int number;
+   //static JButton ok;
+   static JDialog result;
+
     public static void main(String[] args) {
 
-        dict mydict = new dict();
-        //Question myquestion = new Question();
 
+       //final dict mydict = new dict();
+        dict mydict = new dict();
+        number = 1;
         //here I will use:
         JPanel p, p1, p2;
-        JLabel title, title1;
-        JTextField input;
         JButton inputButton;
 
         //Defining my frame
-        JFrame myFrame = new JFrame();
+        final JFrame myFrame = new JFrame();
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setLayout(new BorderLayout() );
-        myFrame = new JFrame("Quiz game");
+        //myFrame = new JFrame("Quiz game");
         myFrame.setBounds(0, 0, 300, 200);
-
 
         //Defining panels
         p = new JPanel();
@@ -44,7 +45,7 @@ public class Main {
 
 
         //Defining Label for question
-            title1 = new JLabel("Question:");//label for 1 panel
+            title1 = new JLabel("Вопрос 1:");//label for 1 panel
             title1.setForeground(Color.BLUE);
             title = new JLabel("Here wiil be the question!");//label for 2 panel
             title.setBounds(60, 7, 200, 30);
@@ -54,7 +55,7 @@ public class Main {
             input.setForeground(Color.GRAY);
             input.setPreferredSize(new Dimension(100,25));
 
-        //очищение инпута при касании курсором
+         //очищение инпута при касании курсором
             input.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
@@ -74,39 +75,95 @@ public class Main {
             inputButton.setBounds(210, 50, 80, 30);
 
 
-         //изменяет содержимое label title при нажатии кнопки
-            inputButton.addActionListener(new ActionListener() {
-                @Override
-            public void actionPerformed(ActionEvent e) {
-                String a = input.getText();
-                //String b = toString();
-                title.setText(mydict.getQuestion(0).getQuestion());
-                input.setText("");
-                if (a.equals("london"))
-                    System.out.println("true");
-                else
-                    System.out.println("false");
-            }
-        });
-
-
-
-
-
             p.add(title1);
             p1.add(title);
             p2.add(input);
             p2.add(inputButton);
 
-        System.out.println(mydict.getQuestion(0));
+
 
             myFrame.setVisible(true);
 
+        //изменяет содержимое label title при нажатии кнопки
+        inputButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (number < 7) number++;
+                int key = mydict.keys.get(mydict.index);
+                String a = input.getText();
+                title1.setText("Вопрос "+ number);
 
-        }
+                if (a.equals(mydict.getQuestion(key).getAnswer())){
+                    System.out.println("true");
+                    mydict.maks ++; }
+
+                if (mydict.index <6){
+                    mydict.index++;
+                    mydict.quiz();
+                    }
 
 
-}
+                else {
+                    //myFrame.setVisible(false);
+                    JPanel pan=new JPanel();
+                    pan.add(new JLabel("Ваш балл " + mydict.maks + "/7"));
+                    pan.add(new JLabel("Хотите пройти тест еще раз?", SwingConstants.CENTER));
+
+                    JButton ok = new JButton("да");
+                    pan.add(ok);
+                    JButton cancel =  new JButton("выход");
+                    pan.add(cancel);
+                    cancel.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent actionEvent) {
+                            myFrame.dispose();
+                        }
+                    });
+
+
+                    ok.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            result.setVisible(false);
+                          //  myFrame.setVisible(false);
+                            mydict.index=0;
+                            number=1;//
+                            mydict.maks=0;
+
+                            dict mydict = new dict();//новый словарь чтоб все смешать
+                            mydict.quiz();
+                           // myFrame.setVisible(true);
+
+                            title1.setText("Вопрос "+ number);
+                            myFrame.repaint();
+                        }});
+
+                    pan.setLayout(new FlowLayout());
+                    pan.setSize(300,300);
+                    //pan.setVisible(true);
+
+
+                    result = new JDialog(myFrame);
+                    result.setSize(300,200);
+                    result.setLocationRelativeTo(myFrame);
+                    result.add(pan);
+                    result.setVisible(true);
+                    System.out.println(mydict.maks);
+                    
+                }}
+
+        });
+        mydict.quiz();
+    }}
+
+
+
+
+
+
+
+
+
 
 
 
--- dict.java
+++ dict.java
@@ -1,31 +1,45 @@
 package com.company;
 import java.util.*;
 
-import java.util.HashMap;
+public class dict  {
 
-public class dict {
+    Main myMain = new Main();
+
     HashMap<Integer, Question> dict = new HashMap<Integer, Question>();
+    static List<Integer> keys;
+    int maks = 0;
+    static int index = 0;
+    static Object Question ;
+    public Object Answer;
 
     public dict() {
-        dict.put(0, new Question("Capital?", "london"));
-        dict.put(1, new Question("Country?", "UK"));
-        dict.put(2, new Question("Region?", "ololo"));
-        dict.put(3, new Question("Language?", "english"));
+        dict.put(0, new Question("<html>Год основания города Пишпек?<br>a)1878<br/>b)1880<br/>c)1879<br/>d)1875</html>", "a"));
+        dict.put(1, new Question("<html>Интернет домен Кыргызстана?<br/>a)kr<br/>b)kz<br/>c)kg<br/>d)kn</html>", "c"));
+        dict.put(2, new Question("<html>Какой писатель является одним из <br/>самых читаемых авторов современности?<br/> <br/>a)Алыкул Осмонов<br/>b)Чынгыз Айтматов<br/>c)Вячеслав Шаповалов<br/>d)Мар Байчиев</html>", "b"));
+        dict.put(3, new Question("<html>Где находится гумбез Манаса?<br/> <br/>a)Талас<br/>b)Жалал-Абад<br/>c)Нарын<br/>d)Чуй</html>", "a"));
+        dict.put(4, new Question("<html>Главный символ горного Кыргызстана?<br/> <br/>a)марал<br/>b)орел<br/>c)снежный барс<br/>d)цветок Айгуль</html>", "c"));
+        dict.put(5, new Question("<html>Единственное месторождение <br/>серебра в Кыргызстане?<br/> <br/>a)Кумтор<br/>b)Солтон-Сары<br/>c)Кызыл-Кия<br/>d)Кумыштаг</html>", "d"));
+        dict.put(6, new Question("<html>Суймонкул Чокморов - знаменитый ... ?<br/> <br/>a)врач<br/>b)поэт,драматург<br/>c)актер, художник<br/>d)режиссер</html>", "c"));
+        keys = new ArrayList<>(dict.keySet());
+        Collections.shuffle(keys);
     }
 
 
-    List keys = new ArrayList(dict.keySet());
-    Collections.shuffle(keys);
-    for (Object o : keys) {
-        // Access keys/values in a random order
-        dict.get(o);
-    }
+
+
 
     public  Question getQuestion(Integer id){
         return dict.get(id);
     }
 
+    public  void quiz(){
 
-
+        myMain.input.setText("");
+        int key = keys.get(index);
+        Question q = dict.get(key);
+        String question  = q.getQuestion();
+        myMain.title.setText(question);
+        //index++;
+    }
 
 }
